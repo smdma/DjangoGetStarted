564d1bcf2cf6d19b3b6ecdf46939fcc32a05e50e
salmond <salmondma@163.com>
2018-07-19 14:47:09 +0800

    3-3：orm介绍与model设计

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UserMessage(models.Model):
+    object_id = models.CharField(primary_key=True, max_length=50, default="", verbose_name="主键")
+    name = models.CharField(max_length=20, null=True, blank=True,
+                            verbose_name=u'用户名')
+    email = models.EmailField(verbose_name=u'邮箱')
+    address = models.CharField(max_length=100, verbose_name=u'联系地址')
+    message = models.CharField(max_length=500, verbose_name=u'留言信息')
+
+    class Meta:
+        verbose_name = u'用户留言信息'
+        verbose_name_plural = u"用户留言信息"
+        ordering = '-object_id'  # 指定默认排序字段
