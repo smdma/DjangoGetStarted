55b24c1b3ee6e93f8fd517501d5fe0f87f978d53
salmond <salmondma@163.com>
2018-07-20 01:21:10 +0800

    3-4：model的增删改数据库

--- apps/message/models.py
+++ apps/message/models.py
@@ -12,4 +12,3 @@ class UserMessage(models.Model):
     class Meta:
         verbose_name = u'用户留言信息'
         verbose_name_plural = u"用户留言信息"
-        ordering = '-object_id'  # 指定默认排序字段
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
+from .models import UserMessage
 
 
 def getform(request):
-    return render(request, 'message_form.html')
+    # all_message = UserMessage.objects.all()
+    #
+    # for message in all_message:
+    #     print(message.name)
 
+    # 处理post请求
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        message = request.POST.get('message', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+
+        # 实例化一个模型对象
+        user_message = UserMessage()
+        # 为对象添加属性
+        # user_message.name = 'salmond2'
+        # user_message.message = '你也帅'
+        # user_message.address = '上海'
+        # user_message.email = 'salmondma@163.com'
+        # user_message.object_id = 'efgh'
+        user_message.name = name
+        user_message.message = message
+        user_message.address = address
+        user_message.email = email
+        user_message.object_id = 'ijkl'
+        # 调用save方法进行保存
+        user_message.save()
+
+    # 删除数据
+    all_message = UserMessage.objects.filter(name='salmond',address='上海')
+    # all_message.delete()
+    for message in all_message:
+        message.delete()
+
+    return render(request, 'message_form.html')
